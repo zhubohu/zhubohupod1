626861b3b6646be6405eadf0e0f02f69b61dcb5b
zhubohu <zhubohu@100tal.com>
2019-11-17 20:40:44 +0800

    init
    Signed-off-by: zhubohu <zhubohu@100tal.com>

---
+++ Example/Pods/Target Support Files/Pods-zhubohupod1_Example/Pods-zhubohupod1_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_zhubohupod1_Example : NSObject
+@end
+@implementation PodsDummy_Pods_zhubohupod1_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-zhubohupod1_Tests/Pods-zhubohupod1_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_zhubohupod1_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_zhubohupod1_Tests
+@end
